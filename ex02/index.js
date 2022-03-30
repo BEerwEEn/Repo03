3650c46c672302dbfe8c4b5ce051dc4b45476a50
BEerwEEn <bitbear33@gmail.com>
2022-03-30 21:48:18 +0200

    adding
    adding ex02

---
+++ ex02/index.js
@@ -0,0 +1,7 @@
+// This is a single line JavaScript comment
+
+/* document.write("This is a multi-line comment!")
+   document.write("This is a multi-line comment!")
+   document.write("This is a multi-line comment!")
+   document.write("This is a multi-line comment!")
+*/
