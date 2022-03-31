504c3dd965b61da28f2ea8766ce03aabbbfcbe67
BEerwEEn <bitbear33@gmail.com>
2022-03-31 03:45:35 +0200

    adding ex15

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+    var numOne, numTwo, difference;
+
+    numOne = 15;
+    numTwo = 8;
+    difference = numOne - numTwo;
+
+    return difference;
+}
+
+console.log (main(25, 8));
+console.log (main(425, 120));
+
+module.exports = main;
+
+
---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+    var numOne, numTwo, product;
+
+    numOne = 15;
+    numTwo = 8;
+    product = numOne * numTwo;
+
+    return product;
+}
+
+console.log (main(25, 8));
+console.log (main(425, 10));
+
+module.exports = main;
+
+
